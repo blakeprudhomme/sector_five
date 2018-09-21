b4f122185f6bb401240eaddd5c90912a53308cb1
Blake Prudhomme <blake.prudhomme@gmail.com>
2018-09-21 08:19:37 -0500

    Add Enemy class
    Add enemy ships that randomly fall from top to bottom

---
+++ enemy.rb
@@ -0,0 +1,20 @@
+# A cpu controlled ship
+class Enemy
+  SPEED = 4
+  IMAGE_RADIUS = 20
+
+  def initialize(window)
+    @image = Gosu::Image.new('images/enemy.png')
+    @radius = IMAGE_RADIUS
+    @x = rand(window.width - 2 * @radius) + @radius
+    @y = 0
+  end
+
+  def draw
+    @image.draw(@x - @radius, @y - @radius, 1)
+  end
+
+  def move
+    @y += SPEED
+  end
+end
--- sector_five.rb
+++ sector_five.rb
@@ -1,8 +1,10 @@
 require 'gosu'
 require_relative 'player'
+require_relative 'enemy'
 
 WIDTH = 800
 HEIGHT = 600
+ENEMY_FREQUENCY = 0.05
 
 TEXT = {
   caption: 'Sector Five'
@@ -14,10 +16,12 @@ class SectorFive < Gosu::Window
     super(WIDTH, HEIGHT)
     self.caption = TEXT[:caption]
     @player = Player.new(self)
+    @enemies = []
   end
 
   def draw
     @player.draw
+    @enemies.each(&:draw)
   end
 
   def update
@@ -25,6 +29,8 @@ class SectorFive < Gosu::Window
     @player.turn_right if button_down?(Gosu::KbRight)
     @player.accelerate if button_down?(Gosu::KbUp)
     @player.move
+    @enemies.push Enemy.new(self) if rand < ENEMY_FREQUENCY
+    @enemies.each(&:move)
   end
 end
 
