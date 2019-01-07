bbcc39326eafd551a76661ea2a261866310ff6bf
blakeprudhomme <blake.prudhomme@gmail.com>
2019-01-07 14:10:00 -0600

    Add Explosion class
    Manipulate an explosion sprite after collisions

--- sector_five.rb
+++ sector_five.rb
@@ -2,6 +2,7 @@ require 'gosu'
 require_relative 'player'
 require_relative 'bullet'
 require_relative 'enemy'
+require_relative 'explosion'
 
 WIDTH = 800
 HEIGHT = 600
@@ -19,12 +20,14 @@ class SectorFive < Gosu::Window
     @player = Player.new(self)
     @enemies = []
     @bullets = []
+    @explosions = []
   end
 
   def draw
     @player.draw
     @enemies.each(&:draw)
     @bullets.each(&:draw)
+    @explosions.each(&:draw)
   end
 
   def update
@@ -53,6 +56,7 @@ class SectorFive < Gosu::Window
         if distance < enemy.radius + bullet.radius
           @enemies.delete(enemy)
           @bullets.delete(bullet)
+          @explosions.push Explosion.new(self, enemy.x, enemy.y)
         end
       end
     end
